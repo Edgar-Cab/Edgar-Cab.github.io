98e250e1de3af64b302287100d150c8e58d7d3b7
Edgar-Cab <84860805+Edgar-Cab@users.noreply.github.com>
2022-08-16 16:49:44 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="x-UA-compatible" content="ie-edge">
+	
+<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	<title>login</title>
+	<meta name="google-site-verification" content="google9b738e708ec71acd.html"/>
+</head>
+<body>
+
+	<center>
+		
+		<form action="logueo.php" method="POST">
+				
+					<input type="text" name="usuario" placeholder="digite correo">
+				<br><br>
+				
+<input type="password" name="contrasenia" placeholder="digite su contraseña">
+				<br><br>
+				<button type="sumbit">ENTRAR</button>
+
+
+		</form>
+
+
+	</center>
+
+
+
+
+</body>
+</html>
