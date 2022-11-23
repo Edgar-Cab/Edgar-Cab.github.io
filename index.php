de67b96b573683882447ee332aee7fc30c8b5a84
Edgar-Cab <84860805+Edgar-Cab@users.noreply.github.com>
2022-11-23 16:55:15 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<title>LOGIN</title> 
+	<meta name="viewport" content="width=device-width, user-scalable=yes, initial-scale=1.0, maximum-scale=3.0, minimum-scale=1.0">
+ <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" >
+	<link rel="stylesheet" href="estilos1.css">
+    
+	<link rel="manifest" href="./manifest.json">
+    <link rel="stylesheet" href="./style.css">
+    <link rel="stylesheet" href="./css/bootstrap.min.css">
+
+</head>  
+<body class="color">
+    <form class="formulario">
+            <h1>Login</h1>
+        <div class="contenedor">
+                <div class="input-contenedor">
+                    <label class="login__label">Matricula</label>
+                    <input type="text" placeholder="Nombre Completo">
+         
+                </div>
+         
+                <div class="input-contenedor">
+                    <label class="login__label">Password</label>
+                    <input type="password" placeholder="Matricula">
+         
+                </div>
+                <div class="w3">
+                <button type="submit" value="Login" class="button">Iniciar Sesion</button></a>
+                </div>
+                
+                <p>¿No tienes una cuenta? <a class="link" href="inicio.php">Home </a></p>
+        </div>
+    </form>
+</body>
+</html>
---
+++ inicio.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aplicacion Web Progresiva</title>
+
+    <link rel="manifest" href="./manifest.json">
+    <link rel="stylesheet" href="./style.css">
+    <link rel="stylesheet" href="./css/bootstrap.min.css">
+    
+</head>
+<body>
+    <header>
+        <h1>Aplicaciones Web Progresivas</h1>
+        <p>Bienvenidos a PWA</p>
+        <a href="index.php">LOGIN</a>
+    </header>
+        
+
+    
+
+    <script src="./script.js" ></script>
+    <script src="./js/bootstrap.min.js" ></script>
+</body>
+</html>
